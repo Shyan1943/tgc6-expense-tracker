43369d9e27e8ec3da3b8961d8aab6214b12a0321
Shyan1943 <shyan_1943@hotmail.com>
2020-07-06 06:26:56 +0000

    Done created expense form & process

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ MONGO_URI = os.environ.get('MONGO_URI')
 client = pymongo.MongoClient(MONGO_URI)
 
 # define my db_name
-DB_NAME = "todolist"
+DB_NAME = "pra_expense"
 
 # read in the SESSION_KEY variable from the operating system environment
 # SESSION_KEY = os.environ.get('SESSION_KEY')
@@ -32,6 +32,23 @@ def home():
 @app.route("/expense/create")
 def expense_create_form():
     return render_template("expense_create_form.template.html")
+
+@app.route("/expense/create", methods=["POST"])
+def expense_create_form_process():
+    expenditure_name = request.form.get("expenditure_name")
+    date = request.form.get("date")
+    transaction_type = request.form.get("transaction_type")
+    reconciled = request.form.get("reconciled")
+
+    client[DB_NAME].expense.insert_one({
+        "expenditure_name" : expenditure_name,
+        "date" : datetime.datetime.strptime(date, "%Y-%m-%d"),
+        "transaction_type" : transaction_type,
+        "reconciled" : reconciled
+    })
+
+    return "Created"
+
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
